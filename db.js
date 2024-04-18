c2ac8be17b1ca0443307241db548ea8b5e9e16c0
Joao Sertoli <jgsertoli@gmail.com>
2024-04-18 11:55:48 -0300

    Refactor database connection and update video endpoint in server.js

--- db.js
+++ db.js
@@ -1,11 +1,9 @@
 import 'dotenv/config'
-
-const postgres = require('postgres');
-require('dotenv').config();
+import postgres from 'postgres'
 
 let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 
-const sql = postgres({
+export const sql = postgres({
     host: PGHOST,
     database: PGDATABASE,
     username: PGUSER,
